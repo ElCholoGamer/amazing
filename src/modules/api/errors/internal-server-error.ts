9b0ea2f55fc3879c28d370fd4f9ed9052edf6b69
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-09 17:13:01 -0500

    refactor(api.errors): remove unused InternalServerError

--- src/modules/api/errors/internal-server-error.ts
+++
@@ -1,7 +0,0 @@
-import { StandardHttpError } from './standard';
-
-export class InternalServerError extends StandardHttpError {
-	public constructor(message = 'Internal server error.') {
-		super(500, message);
-	}
-}
