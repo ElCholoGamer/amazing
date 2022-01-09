968ddaf46ee7002af1a0670739f607f7dc8b2097
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-09 00:40:41 -0500

    feat(utils): add base api handler factory

---
+++ src/common/utils/create-api-handler.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc, { NextConnect, Options } from 'next-connect';
+
+export function createApiHandler<
+	Req extends NextApiRequest = NextApiRequest,
+	Res extends NextApiResponse = NextApiResponse
+>(options: Options<Req, Res> = {}): NextConnect<Req, Res> {
+	options.onError ||= (err, req: Req, res: Res) => {
+		console.error(err);
+		res.status(500).json({
+			statusCode: 500,
+			error: 'Internal server error.',
+		});
+	};
+
+	options.onNoMatch ||= (req: Req, res: Res) => {
+		res.status(404).json({
+			statusCode: 404,
+			error: 'Page not found.',
+		});
+	};
+
+	const handler = nc(options);
+	return handler;
+}
--- src/pages/api/solve.ts
+++ src/pages/api/solve.ts
@@ -1,13 +1,12 @@
-import { NextApiResponse } from 'next';
-import nextConnect from 'next-connect';
 import NextApiFileRequest from '../../common/types/next-api-file-request';
 import { fileUpload } from '../../common/middleware/file-upload';
 import { transformBody } from '../../common/middleware/transform-body';
 import { SolveOptions } from '../../modules/solver/types/solve-options';
 import { solveMaze } from '../../modules/solver/solve';
 import { validateFile } from '../../modules/validator/middleware/validate-file';
+import { createApiHandler } from '../../common/utils/create-api-handler';
 
-const handler = nextConnect();
+const handler = createApiHandler<NextApiFileRequest>();
 
 handler.use(fileUpload('image'));
 handler.use(
@@ -18,7 +17,7 @@ handler.use(
 );
 handler.use(transformBody(SolveOptions));
 
-handler.post<NextApiFileRequest, NextApiResponse>(async (req, res) => {
+handler.post(async (req, res) => {
 	const result = await solveMaze(req.file.buffer, req.body);
 
 	if (result === null) {
