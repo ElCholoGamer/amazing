8a3747ef3fe7dfbeeb6b3670895fc3c26c0f111f
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-09 16:01:26 -0500

    refactor(api.results): remove GET pagination

--- src/pages/api/results/index.ts
+++ src/pages/api/results/index.ts
@@ -1,12 +1,11 @@
-import { RESULT_PAGE_SIZE } from 'common/constants';
+import NextApiFileRequest from 'common/types/next-api-file-request';
 import { UnprocessableEntityError } from 'common/errors/http/unprocessable-entity';
 import { fileUpload } from 'common/middleware/file-upload';
 import { transformBody } from 'common/middleware/transform-body';
-import NextApiFileRequest from 'common/types/next-api-file-request';
 import { createApiHandler } from 'common/utils/create-api-handler';
 import { createResult } from 'modules/database/create-result';
 import { db } from 'modules/database/db';
-import { Result } from 'modules/database/models/result';
+import { getAllResults } from 'modules/database/results';
 import { solveMaze } from 'modules/solver/solve';
 import { SolveOptions } from 'modules/solver/types/solve-options';
 import { validateFile } from 'modules/validator/middleware/validate-file';
@@ -16,17 +15,8 @@ const handler = createApiHandler<NextApiFileRequest>();
 handler.use(db());
 
 handler.get(async (req, res) => {
-	const skipPages = Math.max(Number(req.query.page) - 1, 0) || 0;
-	const results = await Result.find(
-		{},
-		{},
-		{
-			skip: skipPages * RESULT_PAGE_SIZE,
-			limit: RESULT_PAGE_SIZE,
-		}
-	);
-
-	res.json(results);
+	const results = await getAllResults();
+	res.json(results.map(result => result.toPartial()));
 });
 
 handler.post(
