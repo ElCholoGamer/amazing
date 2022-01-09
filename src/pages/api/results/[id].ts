95d94ee57b4a3e6729c0295f67d1a58232d27b2b
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-09 14:12:19 -0500

    feat(api.results): add GET result route

---
+++ src/pages/api/results/[id].ts
@@ -0,0 +1,28 @@
+import { NotFoundError } from 'common/errors/not-found';
+import { createApiHandler } from 'common/utils/create-api-handler';
+import { Result } from 'modules/database/models/result';
+import { db } from 'modules/database/db';
+import { validateObjectId } from 'modules/validator/middleware/validate-object-id';
+
+const handler = createApiHandler();
+
+handler.use(
+	validateObjectId(req => req.query.id, {
+		onFail: () => {
+			throw new NotFoundError('Result not found.');
+		},
+	})
+);
+handler.use(db());
+
+handler.get(async (req, res) => {
+	const { query } = req;
+	const id = Array.isArray(query.id) ? query.id.join(',') : query.id;
+
+	const result = await Result.findById(id);
+	if (!result) throw new NotFoundError('Result not found.');
+
+	res.json(result);
+});
+
+export default handler;
