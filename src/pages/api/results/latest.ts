eabc0a5ed50f165b072ff7a4bccca182d9275c21
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-09 16:01:40 -0500

    feat(api.results): add GET latest route

---
+++ src/pages/api/results/latest.ts
@@ -0,0 +1,14 @@
+import { createApiHandler } from 'common/utils/create-api-handler';
+import { db } from 'modules/database/db';
+import { getLatestResults } from 'modules/database/results';
+
+const handler = createApiHandler();
+
+handler.use(db());
+
+handler.get(async (req, res) => {
+	const results = await getLatestResults();
+	res.json(results.map(result => result.toPartial()));
+});
+
+export default handler;
