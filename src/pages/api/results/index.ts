c0570541917392af96b27b584568af7ca62b1256
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-09 15:21:39 -0500

    refactor(api): move solve route to results

--- src/pages/api/results/index.ts
+++ src/pages/api/results/index.ts
@@ -1,9 +1,17 @@
 import { RESULT_PAGE_SIZE } from 'common/constants';
+import { UnprocessableEntityError } from 'common/errors/http/unprocessable-entity';
+import { fileUpload } from 'common/middleware/file-upload';
+import { transformBody } from 'common/middleware/transform-body';
+import NextApiFileRequest from 'common/types/next-api-file-request';
 import { createApiHandler } from 'common/utils/create-api-handler';
+import { createResult } from 'modules/database/create-result';
 import { db } from 'modules/database/db';
 import { Result } from 'modules/database/models/result';
+import { solveMaze } from 'modules/solver/solve';
+import { SolveOptions } from 'modules/solver/types/solve-options';
+import { validateFile } from 'modules/validator/middleware/validate-file';
 
-const handler = createApiHandler();
+const handler = createApiHandler<NextApiFileRequest>();
 
 handler.use(db());
 
@@ -20,5 +28,30 @@ handler.get(async (req, res) => {
 
 	res.json(results);
 });
+
+handler.post(
+	fileUpload('image'),
+	validateFile({
+		maxSize: 10e6, // 10 MB
+		mimeTypes: ['png', 'jpeg', 'jpg', 'webp'].map(ext => `image/${ext}`),
+	}),
+	transformBody(SolveOptions),
+	async (req, res) => {
+		const steps = await solveMaze(req.file.buffer, req.body);
+
+		if (steps === null) {
+			throw new UnprocessableEntityError('Unable to solve maze');
+		}
+
+		const result = await createResult(steps);
+		res.json(result);
+	}
+);
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
 
 export default handler;
