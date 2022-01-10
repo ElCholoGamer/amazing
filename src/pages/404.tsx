b86884d9d39dbf77b1fcfc8c054347192d690874
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-10 14:35:44 -0500

    feat(pages): edit 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,20 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Button from '@mui/material/Button';
 import { Layout } from 'modules/layout/components/Layout';
+import styles from 'styles/NotFound.module.scss';
 
 const NotFound: React.FC = () => {
 	const router = useRouter();
 
 	return (
 		<Layout title="Not found">
-			<main className="m-5">
-				<h1>Page not found :(</h1>
+			<main className={styles.container}>
+				<h2>404 - Page not found :(</h2>
+
+				<p>(Maybe you mispelled something?)</p>
 				<br />
 
-				<Link href="/" passHref>
-					<Button className="no-transform m-2" variant="contained">
-						Home
-					</Button>
-				</Link>
-				<br />
-				<Button className="no-transform m-2" onClick={() => router.back()}>
+				<Button className="no-transform" variant="contained" onClick={() => router.back()}>
 					Go back
 				</Button>
 			</main>
