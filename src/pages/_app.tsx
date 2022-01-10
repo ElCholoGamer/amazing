c1172796b618122b02f34e53f96333e8c87be793
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-10 14:46:45 -0500

    feat: use bootstrap utilities

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap-utilities.css';
 import 'styles/globals.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
