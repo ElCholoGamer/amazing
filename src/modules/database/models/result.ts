fb90360e25ca5abae60e728e17810d01d7ea58b6
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-10 12:43:55 -0500

    fix(database): add Result model type

--- src/modules/database/models/result.ts
+++ src/modules/database/models/result.ts
@@ -1,5 +1,6 @@
-import mongoose, { model } from 'mongoose';
+import mongoose, { model, Model } from 'mongoose';
 import { IResult, ResultSchema } from '../schemas/result';
 
 export const MODEL_NAME = 'Result';
-export const Result = mongoose.models[MODEL_NAME] || model<IResult>(MODEL_NAME, ResultSchema);
+export const Result: Model<IResult> =
+	mongoose.models[MODEL_NAME] || model<IResult>(MODEL_NAME, ResultSchema);
