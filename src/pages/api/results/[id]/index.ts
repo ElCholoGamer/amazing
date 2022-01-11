818e9ef6e9c4b5487a0c10fd36b63ad2dd9645bd
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-11 00:01:00 -0500

    refactor(api): use getResult function

--- src/pages/api/results/[id]/index.ts
+++ src/pages/api/results/[id]/index.ts
@@ -1,24 +1,17 @@
 import { NotFoundError } from 'modules/api/errors/not-found';
 import { createApiHandler } from 'modules/api/create-api-handler';
-import { Result } from 'modules/database/models/result';
 import { db } from 'modules/database/middleware/db';
-import { validateObjectId } from 'modules/validator/middleware/validate-object-id';
+import { getResult } from 'modules/database/result';
 
 const handler = createApiHandler();
 
-handler.use(
-	validateObjectId(req => req.query.id, {
-		onFail: () => {
-			throw new NotFoundError('Result not found.');
-		},
-	})
-);
 handler.use(db());
 
 handler.get(async (req, res) => {
 	const id = req.queryString('id');
+	if (!id) throw new Error('ID query string not found');
 
-	const result = await Result.findById(id);
+	const result = await getResult(id);
 	if (!result) throw new NotFoundError('Result not found.');
 
 	res.json(result);
