4b51bc00a46ad05610baab6f6ba1e1e9454a86fe
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-10 22:26:48 -0500

    refactor(styles): use bootstrap theme color variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap-utilities.css';
 import 'styles/globals.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
