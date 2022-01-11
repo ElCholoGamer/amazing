46f93ad8eb852db35f21ca83b9501117b48696ab
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-10 23:59:41 -0500

    feat(layout): add mui theme provider

---
+++ src/modules/layout/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material';
+import variables from 'styles/variables.module.scss';
+console.log('Var:', variables);
+
+export const createAppTheme = () =>
+	createTheme({
+		palette: {
+			primary: { main: variables.primary },
+			secondary: { main: variables.secondary },
+			error: { main: variables.danger },
+			warning: { main: variables.warning },
+			info: { main: variables.info },
+			success: { main: variables.success },
+			mode: 'dark',
+		},
+		typography: {
+			fontFamily: ['-apple-system', 'Roboto', 'Arial', 'sans-serif'].join(','),
+		},
+	});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import { useMemo } from 'react';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material';
+import { createAppTheme } from 'modules/layout/theme';
 import 'styles/globals.scss';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+	const theme = useMemo(() => createAppTheme(), []);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
+};
 
 export default App;
