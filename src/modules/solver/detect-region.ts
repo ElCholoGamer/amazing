3383074b0b7e367faa05702f39ced5a05aedf813
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-11 17:08:33 -0500

    feat(api): add maze region detection route

---
+++ src/modules/solver/detect-region.ts
@@ -0,0 +1,68 @@
+import { Coordinate } from 'common/types/coordinate';
+import { bufferToImageData } from 'common/utils/buffer-to-image-data';
+import { parseWalls } from 'modules/parser/parse-walls';
+import { Region } from 'sharp';
+
+export async function detectMazeRegion(image: Buffer): Promise<Region> {
+	const imageData = await bufferToImageData(image, null);
+	const wallData = parseWalls(imageData);
+
+	const wallCountsV = wallData.map(col => col.reduce((count, wall) => count + +!wall, 0));
+	const wallCountsH = [...Array(wallData[0].length)].map((_, y) =>
+		wallData.reduce((count, col) => count + +!col[y], 0)
+	);
+
+	console.log(wallData.map(col => col[0]).flat());
+
+	const center: Coordinate = {
+		x: Math.floor(wallData.length / 2),
+		y: Math.floor(wallData[0].length / 2),
+	};
+
+	let left = 0;
+	let leftX = 0;
+
+	for (let x = leftX; x < center.x; x++) {
+		if (wallCountsV[x] > left) {
+			left = wallCountsV[x];
+			leftX = x;
+		}
+	}
+
+	let right = 0;
+	let rightX = wallData.length - 1;
+
+	for (let x = rightX; x >= center.x; x--) {
+		if (wallCountsV[x] > right) {
+			right = wallCountsV[x];
+			rightX = x;
+		}
+	}
+
+	let top = 0;
+	let topY = 0;
+
+	for (let y = topY; y < center.y; y++) {
+		if (wallCountsH[y] > top) {
+			top = wallCountsH[y];
+			topY = y;
+		}
+	}
+
+	let bottom = 0;
+	let bottomY = wallData[0].length - 1;
+
+	for (let y = bottomY; y >= center.y; y--) {
+		if (wallCountsH[y] > bottom) {
+			bottom = wallCountsH[y];
+			bottomY = y;
+		}
+	}
+
+	return {
+		left: leftX,
+		top: topY,
+		width: rightX - leftX + 1,
+		height: bottomY - topY + 1,
+	};
+}
---
+++ src/pages/api/detect/region.ts
@@ -0,0 +1,28 @@
+import { fileUpload } from 'common/middleware/file-upload';
+import { IMAGE_MIME_TYPES, MAX_IMAGE_SIZE } from 'modules/api/constants';
+import { createApiHandler } from 'modules/api/create-api-handler';
+import { NextApiFileRequest } from 'modules/api/types/next-api-file-request';
+import { detectMazeRegion } from 'modules/solver/detect-region';
+import { validateFile } from 'modules/validator/middleware/validate-file';
+
+const handler = createApiHandler<NextApiFileRequest>();
+
+handler.post(
+	fileUpload('image'),
+	validateFile({
+		maxSize: MAX_IMAGE_SIZE,
+		mimeTypes: IMAGE_MIME_TYPES,
+	}),
+	async (req, res) => {
+		const region = await detectMazeRegion(req.file.buffer);
+		res.json(region);
+	}
+);
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+export default handler;
