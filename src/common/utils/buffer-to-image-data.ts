446bcf126ac88fb5844b00ed9b07cee2ce9873ca
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-11 17:07:44 -0500

    refactor(utils): make imageData extract region optional

--- src/common/utils/buffer-to-image-data.ts
+++ src/common/utils/buffer-to-image-data.ts
@@ -1,12 +1,30 @@
 import sharp, { Region } from 'sharp';
 import { ImageData } from '../types/image-data';
 
-export async function bufferToImageData(buffer: Buffer, region: Region): Promise<ImageData> {
-	const pixelBuffer = await sharp(buffer).extract(region).ensureAlpha().raw().toBuffer();
+export async function bufferToImageData(buffer: Buffer, region?: Region): Promise<ImageData> {
+	const sharpInstance = sharp(buffer);
+	if (region) sharpInstance.extract(region);
+
+	const pixelBuffer = await sharpInstance.ensureAlpha().raw().toBuffer();
+
+	let width: number;
+	let height: number;
+
+	if (region) {
+		width = region.width;
+		height = region.height;
+	} else {
+		const metadata = await sharp(buffer).metadata();
+		if (!metadata.width || !metadata.height)
+			throw new Error('Could not extract image size metadata');
+
+		width = metadata.width;
+		height = metadata.height;
+	}
 
 	return {
-		width: region.width,
-		height: region.height,
+		width,
+		height,
 		pixelData: new Uint8ClampedArray(pixelBuffer),
 	};
 }
