6c85973cdc87de75f32bfc5350aaaf99b6464fa6
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-11 01:49:00 -0500

    feat(layout): add responsive header menu

--- src/modules/layout/components/Header.tsx
+++ src/modules/layout/components/Header.tsx
@@ -1,21 +1,68 @@
 import Link from 'next/link';
 import Button from '@mui/material/Button';
-import { Navigation } from './Navigation';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/MenuRounded';
 import styles from 'styles/Header.module.scss';
+import Typography from '@mui/material/Typography';
+import { MouseEvent, useState } from 'react';
 
-export const Header: React.FC = () => (
-	<header className={styles.container} role="banner">
-		<h1>
-			<Link href="/">Amazing</Link>
-		</h1>
+export const Header: React.FC = () => {
+	const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+	const open = anchorEl !== null;
 
-		<Navigation className={styles.navigation}>
-			<Link href="/solve">Solve a maze</Link>
-			<Link href="/about">About</Link>
-		</Navigation>
+	const links = [
+		{ href: '/solve', label: 'Solve a maze' },
+		{ href: '/about', label: 'About' },
+	];
 
-		<Link href="https://github.com/ElCholoGamer/amazing" passHref>
-			<Button className={`${styles.rightLink} no-transform`}>View on GitHub</Button>
-		</Link>
-	</header>
-);
+	const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(e.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<header className={`${styles.container} p-2 p-sm-4`} role="banner">
+			<Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
+				<Button onClick={handleClick}>
+					<MenuIcon fontSize="large" />
+				</Button>
+				<nav className={styles.navigation} role="navigation">
+					<Menu anchorEl={anchorEl} onClose={handleClose} open={open}>
+						{links.map(link => (
+							<MenuItem key={link.href}>
+								<Typography>
+									<Link href={link.href}>{link.label}</Link>
+								</Typography>
+							</MenuItem>
+						))}
+					</Menu>
+				</nav>
+			</Box>
+
+			<h1 className="mx-2">
+				<Link href="/">Amazing</Link>
+			</h1>
+
+			<Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+				<nav className={`${styles.navigation} mx-3`} role="navigation">
+					<ul>
+						{links.map(link => (
+							<li key={link.href}>
+								<Link href={link.href}>{link.label}</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+			</Box>
+
+			<Link href="https://github.com/ElCholoGamer/amazing" passHref>
+				<Button className={`${styles.rightLink} no-transform text-end`}>View on GitHub</Button>
+			</Link>
+		</header>
+	);
+};
