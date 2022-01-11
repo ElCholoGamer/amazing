459a62d6b9e3a4dcf9068a8daca2c34be3bbb3f7
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-11 01:46:27 -0500

    fix(pages): import bootstrap utilities from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material';
 import { createAppTheme } from 'modules/layout/theme';
+import 'bootstrap/dist/css/bootstrap-utilities.css';
 import 'styles/globals.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
