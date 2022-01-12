63b430ec9abb0d637d93f07cb9ef418506616495
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-12 00:10:45 -0500

    feat(parser): add simple noise remover

--- src/modules/parser/parse-walls.ts
+++ src/modules/parser/parse-walls.ts
@@ -3,10 +3,10 @@ import { ImageData } from 'common/types/image-data';
 
 export function parseWalls(imageData: ImageData): boolean[][] {
 	const { width, height, pixelData } = imageData;
-	const walls: boolean[][] = [];
+	const noisedWalls: boolean[][] = [];
 
 	for (let x = 0; x < width; x++) {
-		walls[x] = [];
+		noisedWalls[x] = [];
 
 		for (let y = 0; y < height; y++) {
 			const index = (y * width + x) * 4;
@@ -14,12 +14,29 @@ export function parseWalls(imageData: ImageData): boolean[][] {
 
 			if (a < WALL_THRESHOLD) {
 				// Transparent pixel = Not a wall
-				walls[x][y] = false;
+				noisedWalls[x][y] = false;
 				continue;
 			}
 
 			const luminosity = (r + g + b) / 3;
-			walls[x][y] = luminosity < WALL_THRESHOLD;
+			noisedWalls[x][y] = luminosity < WALL_THRESHOLD;
+		}
+	}
+
+	const walls: typeof noisedWalls = [];
+
+	for (let x = 0; x < noisedWalls.length; x++) {
+		walls[x] = [];
+
+		for (let y = 0; y < noisedWalls[x].length; y++) {
+			let neighbors = 0;
+
+			if (x > 0 && noisedWalls[x - 1][y]) neighbors++;
+			if (x < noisedWalls.length - 1 && noisedWalls[x + 1][y]) neighbors++;
+			if (y > 0 && noisedWalls[x][y - 1]) neighbors++;
+			if (y < noisedWalls[x].length - 1 && noisedWalls[x][y + 1]) neighbors++;
+
+			walls[x][y] = noisedWalls[x][y] && neighbors > 1;
 		}
 	}
 
